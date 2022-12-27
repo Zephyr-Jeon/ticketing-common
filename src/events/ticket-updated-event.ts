6bc6c1ac5972eaaa5f14ff08e8298587b5ec7c5c
Zephyr.Jeon <zephyr.jeon@odcode.com>
2022-12-27 09:50:48 -0800

    Updates

---
+++ src/events/ticket-updated-event.ts
@@ -0,0 +1,12 @@
+import { Event } from './base-listener';
+import { Subjects } from './subjects';
+
+export interface TicketUpdatedEvent extends Event {
+  subject: Subjects.TicketUpdated;
+  data: {
+    id: string;
+    title: string;
+    price: number;
+    userId: string;
+  };
+}
