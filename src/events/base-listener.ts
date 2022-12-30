4c771ec635d3bcc324f5f293b386d35ba72b2626
Zephyr.Jeon <zephyr.jeon@odcode.com>
2022-12-30 12:29:49 -0800

    Updates

--- src/events/base-listener.ts
+++ src/events/base-listener.ts
@@ -10,6 +10,7 @@ export abstract class Listener<T extends Event> {
   /* Name of the channel this listener is going to listen to */
   abstract subject: T['subject'];
   /* Name of the queue group this listener will join */
+  /* Queue group makes sure multiple instances of the same service are not all going to recevice the same event */
   abstract queueGroupName: string;
   /* Code to set up the subscription */
   abstract onMessage(data: T['data'], msg: Message): void;
@@ -26,17 +27,17 @@ export abstract class Listener<T extends Event> {
   subscriptionOptions() {
     return this.client
       .subscriptionOptions()
-      .setDeliverAllAvailable()
       .setManualAckMode(true)
       .setAckWait(this.ackWait)
-      .setDurableName(this.queueGroupName);
+      .setDeliverAllAvailable()
+      .setDurableName(this.queueGroupName); // when subsriber
   }
 
   /* Function to run when a message is reveiced */
   listen() {
     const subscription = this.client.subscribe(
       this.subject,
-      this.queueGroupName,
+      this.queueGroupName, // 1. only one of subscribers with the same queue group name is going to receive each event, 2. along with setDeliverAllAvailable, setDurableName options, makes sure not to remove the durable subscription when disconnected
       this.subscriptionOptions()
     );
 
